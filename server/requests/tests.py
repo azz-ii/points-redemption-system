a9aefc5792e948a6f1c815b3efef9340fbce58cd
markusbautista <markus.015@outlook.com>
2026-05-14 11:02:43 +0800

    AR test 3

--- server/requests/tests.py
+++ server/requests/tests.py
@@ -343,7 +343,6 @@ class MediaServingTests(TestCase):
         except Exception:
             pass
 
-    def test_media_url_serves_pdf(self):
     @override_settings(DEBUG=True)
     def test_media_url_serves_pdf(self):
         url = f'/media/{self.rel_dir.replace('\\', '/')}/{self.filename}'
